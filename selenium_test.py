14c91cc497f526c118c7515f3b7e917af6e9771c
yaqinz <zyqwenny@gmail.com>
2021-03-08 19:24:17 -0500

    add log

--- selenium_test.py
+++ selenium_test.py
@@ -1,19 +1,19 @@
 #! /usr/bin/python3
 import traceback
-from argparse import ArgumentParser
+import logging
+# from argparse import ArgumentParser
 from random import randint, random
-from threading import Lock, Thread
+# from threading import Lock, Thread
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 # from pyvirtualdisplay import Display
 from selenium.webdriver.firefox.options import Options
 from yaml import safe_load
+import sys
 
 with open('resources/config.yaml') as yaml_in:
     config = safe_load(yaml_in)
-
-
 class BrowserTest:
     def __init__(self):
         # self.browser = webdriver.Chrome(executable_path='E:/cs6620/proj/raw/driver/chromedriver.exe')
@@ -31,8 +31,12 @@ class BrowserTest:
             sleep(randint(5, 10) + random())
             assert 'URL Shortener' in self.browser.title
             print("Pass openhome")
-        except AssertionError as a:
+        except Exception as e:
+            logging.error("Failed open_home!!!"+ config['selenium_test_url'] 
+            +"\n" + traceback.format_exc())
             print("Failed open_home!!!")
+            sys.exit()
+
 
     # test if the warning shows when the input is empty. Need some fix.
     def check_empty_link(self):
@@ -48,11 +52,12 @@ class BrowserTest:
                 'cloud_url_output')) == 0
             self.browser.find_element_by_class_name('btn.btn-primary').click()
             assert self.browser.find_element_by_class_name(
-                'invalid-feedback').is_displayed() == False
+                'invalid-feedback').is_displayed() == True
             assert len(self.browser.find_elements_by_id(
                 'cloud_url_output')) == 0
             print("Pass check empty link")
-        except AssertionError as a:
+        except AssertionError as e:
+            logging.error("Falied check_empty_link\n" + traceback.format_exc())
             print("Falied check_empty_link")
 
     def check_regular_link(self):
@@ -75,7 +80,9 @@ class BrowserTest:
             self.browser.find_element_by_id('cloud_url_input').clear()
             sleep(randint(1, 3) + random())
             print("Pass testcase 1 in regular link")
-        except AssertionError as a:
+        except AssertionError as e:
+            logging.error("regular_link 1: https://github.com/yachinz/format_test\n" 
+            + traceback.format_exc())
             print("Falied, regular_link 1: https://github.com/yachinz/format_test")
 
         # testcase2
@@ -94,7 +101,9 @@ class BrowserTest:
             self.browser.find_element_by_id('cloud_url_input').clear()
             sleep(randint(1, 3) + random())
             print("Pass testcase 2 in regular link")
-        except AssertionError as a:
+        except AssertionError as e:
+            logging.error("regular_link 2: https://github.com/yachinz/live2d-widget\n" 
+            + traceback.format_exc())
             print("Falied, regular_link 2: https://github.com/yachinz/live2d-widget")
 
         # testcase3
@@ -113,7 +122,9 @@ class BrowserTest:
             self.browser.find_element_by_id('cloud_url_input').clear()
             sleep(randint(1, 3) + random())
             print("Pass testcase 3 in regular link")
-        except AssertionError as a:
+        except AssertionError as e:
+            logging.error("regular_link 3: https://github.com/yachinz/easy-animator\n" 
+            + traceback.format_exc())
             print("Falied, regular_link 3: https://github.com/yachinz/easy-animator")
 
         # testcase4
@@ -132,7 +143,9 @@ class BrowserTest:
             self.browser.find_element_by_id('cloud_url_input').clear()
             sleep(randint(1, 3) + random())
             print("Pass testcase 4 in regular link")
-        except AssertionError as a:
+        except AssertionError as e:
+            logging.error("regular_link 4: github.com/yachinz/live2d-widget\n" 
+            + traceback.format_exc())
             print("Falied, regular_link 4: github.com/yachinz/live2d-widget")
 
     def check_redirect(self):
@@ -146,9 +159,13 @@ class BrowserTest:
             sleep(randint(5, 10) + random())
             assert self.browser.current_url == 'https://github.com/yachinz/format_test'
             print("Pass testcase 1 in check_redirect")
-        except AssertionError as a:
+        except AssertionError as e:
+            logging.error("redirect_link 1: http://localhost:5000/aloEv2, " 
+            + "https://github.com/yachinz/format_test\n" 
+            + traceback.format_exc())
             print(
-                "Falied, redirect_link 1: http://localhost:5000/aloEv2, https://github.com/yachinz/format_test")
+                "Falied, redirect_link 1: http://localhost:5000/aloEv2, " 
+                + "https://github.com/yachinz/format_test")
 
         # testcase2
         # https://github.com/yachinz/live2d-widget
@@ -159,7 +176,11 @@ class BrowserTest:
             assert self.browser.current_url == 'https://github.com/yachinz/live2d-widget'
             print("Pass testcase 2 in check_redirect")
         except AssertionError as a:
-            print("Failed, redirect_link 2: http://localhost:5000/4nSynq, https://github.com/yachinz/live2d-widget")
+            logging.error("Falied, redirect_link 2: http://localhost:5000/4nSynq, "
+            + "https://github.com/yachinz/live2d-widget\n" 
+            + traceback.format_exc())
+            print("Failed, redirect_link 2: http://localhost:5000/4nSynq, " 
+            + "https://github.com/yachinz/live2d-widget")
 
         # testcase3
         # https://github.com/yachinz/easy-animator
@@ -170,7 +191,11 @@ class BrowserTest:
             assert self.browser.current_url == 'https://github.com/yachinz/easy-animator'
             print("Pass testcase 3 in check_redirect")
         except AssertionError as a:
-            print("Failed, redirect_link 3: http://localhost:5000/1JbS5c, https://github.com/yachinz/easy-animator")
+            logging.error("redirect_link 3: http://localhost:5000/1JbS5c, " 
+            + "https://github.com/yachinz/easy-animator\n" 
+            + traceback.format_exc())
+            print("Failed, redirect_link 3: http://localhost:5000/1JbS5c, " 
+            + "https://github.com/yachinz/easy-animator")
 
         # testcase4
         # github.com/yachinz/live2d-widget
@@ -181,7 +206,11 @@ class BrowserTest:
             assert self.browser.current_url == 'https://github.com/yachinz/live2d-widget'
             print("Pass testcase 4 in check_redirect")
         except AssertionError as a:
-            print("Failed, redirect_link 4: http://localhost:5000/6UErY0, https://github.com/yachinz/live2d-widget")
+            logging.error("Falied, redirect_link 4: http://localhost:5000/6UErY0, " 
+            + "https://github.com/yachinz/live2d-widget\n" 
+            + traceback.format_exc())
+            print("Failed, redirect_link 4: http://localhost:5000/6UErY0, " 
+            + "https://github.com/yachinz/live2d-widget")
 
     def integration(self):
         print("----------------------------------------------")
@@ -191,7 +220,7 @@ class BrowserTest:
             sleep(randint(1, 3) + random())
             # Input the url link.
             self.browser.find_element_by_id('cloud_url_input').send_keys(
-                "https://github.com/yachinz/format_test")
+                "https://www.northeastern.edu/")
             sleep(randint(1, 3) + random())
 
             # Click the submit Button.
@@ -206,9 +235,10 @@ class BrowserTest:
                 'cloud_url_output').get_attribute("value")
             self.browser.get(shortURL)
             sleep(randint(1, 3) + random())
-            assert self.browser.current_url == 'https://github.com/yachinz/format_test'
+            assert self.browser.current_url == 'https://www.northeastern.edu/'
             print('Pass, integration test')
         except Exception:
+            logging.error("Falied, integration test: https://www.northeastern.edu/\n" + traceback.format_exc())
             print("Failed, integration test!!!")
 
     def close_browser(self):
@@ -257,6 +287,9 @@ class ArgParser:
 
 
 if __name__ == '__main__':
+    logging.basicConfig(filename='log_record.log',
+    level=logging.ERROR, filemode='w', format='[%(asctime)s] [%(levelname)s] >>>  %(message)s',
+                    datefmt='%Y-%m-%d %I:%M:%S')
     new_browser = BrowserTest()
     new_browser.open_home()
     new_browser.check_empty_link()
